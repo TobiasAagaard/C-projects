0e36f94860bb78758ccff611fee4b8fe981b4e55
Tobias Aagaard <101559233+TobiasAagaard@users.noreply.github.com>
2024-10-24 16:19:07 +0200

    Days with suffixes

--- dates-switches/date.c
+++ dates-switches/date.c
@@ -50,9 +50,25 @@ int main(void) {
     printf("This is not a month");
   } 
 
-  printf("%d", day);
+  printf(" %d", day);
  
-  
+  switch (day) {
+    case 1: case 21: case 31:
+    printf("st");
+    break;
+    case 2: case 22:
+    printf("nd");
+    break;
+    case 3: case 23:
+    printf("rd");
+    break;
+    case 4: case 5: case 6: case 7: case 8: case 9: case 10: case 11: case 12: case 13: case 14: case 15: case 16: case 17: case 18: case 19: case 20: case 24: case 25: case 26: case 27: case 28: case 29: case 30:
+    printf("th");
+    break;
+    default:
+    printf("This is not a day in the month!");
+  } 
+
   
   return 0;
 }
