077fabdb782399a36ff6f1afa3f7cd63734a5315
Tobias Aagaard <101559233+TobiasAagaard@users.noreply.github.com>
2024-10-24 10:36:33 +0200

    Switches

---
+++ dates-switches/date.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int main(void) {
+  int month, day, year;
+
+
+  printf("Enter date (mm/dd/yyyy): ");
+
+  scanf("%d /%d /%d", &month, &day, &year);
+
+
+  switch (month) {
+    case 1:
+    printf("January");
+    break;
+    case 2:
+    printf("February");
+    break;
+    case 3: 
+    printf("March");
+    break;
+    case 4: 
+    printf("April");
+    break;
+    case 5: 
+    printf("May");
+    break;
+    case 6:
+    printf("June");
+    break;
+    case 7: 
+    printf("July");
+    case 8: 
+    printf("August");
+    break;
+    case 9:
+    printf("September");
+    break;
+    case 10:
+    printf("October");
+    break;
+    case 11:
+    printf("November");
+    break;
+    case 12:
+    printf("December");
+    break;
+    default: 
+    printf("This is not a month");
+  } 
+
+  printf("%d", day);
+ 
+  
+  
+  return 0;
+}
